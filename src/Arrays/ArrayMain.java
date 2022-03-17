3e60d4b9419e36f2d1c7f75f01b519cee58c1a83
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2022-03-17 10:36:44 +0530

    Initial push for Java Track String Assignment 1.

---
+++ src/Arrays/ArrayMain.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        int n = 0;
+//        int arr [] = new int[n];
+        int arr[] = {1,3,-1,2,-3};
+        calculateSubArrays(arr);
+    }
+
+    private static void calculateSubArrays(int[] arr) {
+        int count = 0;
+        int n = arr.length;
+
+        System.out.println(count);
+    }
+}
---
+++ src/String/StringMain.java
@@ -0,0 +1,60 @@
+package String;
+
+import java.util.Scanner;
+
+public class StringMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first string :- ");
+        String first_str = scanner.nextLine();
+        System.out.print("Enter second string :- ");
+        String second_str = scanner.nextLine();
+        System.out.print("Enter shuffle string :- ");
+        String shuffle_str = scanner.nextLine();
+
+        String valid_str = "";
+
+        int i = 0;
+        int j = 0;
+        boolean first = false;
+        boolean second = true;
+
+        while(i<first_str.length() && j<second_str.length()){
+
+            if(first){
+                valid_str += String.valueOf(first_str.charAt(i));
+                i++;
+                first = false;
+                second = true;
+            }
+            else if(second){
+                valid_str += String.valueOf(second_str.charAt(j));
+                j++;
+                first = true;
+                second = false;
+            }
+        }
+
+        while (i<first_str.length()){
+            valid_str+= String.valueOf(first_str.charAt(i));
+            i++;
+        }
+        while (j<second_str.length()){
+            valid_str+= String.valueOf(first_str.charAt(j));
+            j++;
+        }
+
+
+        System.out.println(valid_str);
+
+        if(valid_str.equals(shuffle_str)){
+            System.out.println("Valid Input");
+        }else{
+            System.out.println("Invalid Input");
+        }
+
+
+    }
+}
